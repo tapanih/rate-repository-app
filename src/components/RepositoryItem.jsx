a9d66b4f0689cc053ad6c4837c4def2efab9a3ba
Tapani Honkanen <tapani.honkanen@hotmail.com>
2020-12-11 00:43:12 +0200

    Exercise 10.5

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -3,66 +3,83 @@ import { Image, View, StyleSheet } from 'react-native';
 import theme from '../theme';
 import Text from './Text';
 
-const headerStyles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
+    padding: 10,
+    marginBottom: 2,
+    backgroundColor: theme.colors.light
+  },
+  infoContainer: {
     flexDirection: 'row',
     flexGrow: 1,
-  },
-  logo: {
-    width: 50,
-    height: 50,
-  },
-  language: {
-    backgroundColor: theme.colors.primary
   },
   avatarContainer: {
     flexGrow: 0,
     paddingRight: 15,
   },
-  infoContainer: {
+  descContainer: {
     flexGrow: 1,
+    flexShrink: 1,
   },
-});
-
-const RepositoryItemHeader = ({ item }) => {
-  return (
-    <View style={headerStyles.container}>
-      <View style={headerStyles.avatarContainer}>
-        <Image
-          source={{uri: item.ownerAvatarUrl}}
-          style={headerStyles.logo}
-        />
-      </View>
-      <View style={headerStyles.infoContainer}>
-        <Text>{item.fullName}</Text>
-        <Text>{item.description}</Text>
-      </View>
-      </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 10,
-    marginBottom: 10,
-    backgroundColor: theme.colors.light
+  statContainer: {
+    marginTop: 10,
+    flexDirection: 'row',
+    flexGrow: 1,
+    justifyContent: 'space-evenly',
+  },
+  logo: {
+    width: 60,
+    height: 60,
   },
   language: {
+    textAlign: 'center',
+    color: theme.colors.light,
     backgroundColor: theme.colors.primary,
     borderColor: theme.colors.primary,
-    borderWidth: 5,
-    borderRadius: 20,
+    margin: 2,
+    borderTopWidth: 2,
+    borderBottomWidth: 2,
+    borderWidth: 4,
+    borderRadius: 5,
     alignSelf: 'flex-start'
-  }
+  },
+  text: {
+    margin: 4,
+  },
 });
+
+const Stat = ({ value, description }) => {
+  value = value >= 1000 ? Math.round(value / 100) / 10 + "k" : value;
+  return (
+    <View style={{ flexGrow: 0 }}>
+      <Text style={{textAlign: 'center'}} fontWeight='bold'>{value}</Text>
+      <Text>{description}</Text>
+    </View>
+  );
+};
 
 const RepositoryItem = ({ item }) => {
   return (
     <View style={styles.container}>
-      <RepositoryItemHeader item={item} />
-      <Text style={styles.language}>
-        Ketan
-      </Text>
+      <View style={styles.infoContainer}>
+        <View style={styles.avatarContainer}>
+          <Image
+            source={{uri: item.ownerAvatarUrl}}
+            style={styles.logo}
+          />
+        </View>
+        <View style={styles.descContainer}>
+          <Text style={styles.text} fontWeight="bold" fontSize="subheading">{item.fullName}</Text>
+          <Text style={styles.text} color="textSecondary">{item.description}</Text>
+          <Text style={styles.language}>{item.language}</Text>
+        </View>
+      </View>
+      <View style={styles.statContainer}>
+        <Stat value={item.stargazersCount} description='Stars' />
+        <Stat value={item.forksCount} description='Forks'/>
+        <Stat value={item.reviewCount} description='Reviews'/>
+        <Stat value={item.ratingAverage} description='Rating'/>
+      </View>
     </View>
   );
 };
